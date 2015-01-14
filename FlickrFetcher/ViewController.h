2c03c70c7e946b90e0465415087f7e3def010f21
AlgorithmicAscension <AlgorithmicAscension@gmail.com>
2015-01-14 16:03:33 -0500

    Initial Commit

---
+++ FlickrFetcher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlickrFetcher
+//
+//  Created by Benjamin Chen on 1/14/15.
+//  Copyright (c) 2015 Benjamin Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FlickrFetcher/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlickrFetcher
+//
+//  Created by Benjamin Chen on 1/14/15.
+//  Copyright (c) 2015 Benjamin Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
